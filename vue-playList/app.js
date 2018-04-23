a3c468c93f23ae0f7538681034ff13488a2128a6
gangya0327 <king9999427@163.com>
2018-04-23 17:36:16 +0800

    vue

---
+++ vue-playList/app.js
@@ -0,0 +1,27 @@
+//实例化Vue对象
+new Vue({
+    el: '#app', //element 需要获取的元素，根容器
+    data: { //用于数据的存储
+        name: 'raven',
+        website: 'http://www.baidu.com',
+        websiteTag: '<a href="http://www.baidu.com">百度我</a>',
+        age: 20,
+        x: 0,
+        y: 0
+    },
+    methods: { //存储函数
+        greet: function(time) {
+            return 'good ' + time + ' ' + this.name + '!';
+        },
+        add: function(inc) {
+            this.age += inc
+        },
+        subtract: function(dec) {
+            this.age -= dec
+        },
+        updateXY: function(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
+        }
+    }
+})
