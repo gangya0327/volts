70089c68f7f41aa74fb54ff94cbff76c724d8dc1
raven <king9999427@163.com>
2018-04-28 02:19:27 +0800

    users

--- vue-second/src/App.vue
+++ vue-second/src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <users/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import Users from './components/Users'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Users
   }
 }
 </script>
---
+++ vue-second/src/components/Users.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Users</h1>
+    <ul>
+      <li v-for='user in users'>{{user.name}}: {{user.email}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "users",
+  data() {
+    return {
+      users: [
+        {
+          name: "peter",
+          email: "peter4499427@163.cmo",
+          contacted: false
+        },
+        {
+          name: "mike",
+          email: "mike9529427@163.cmo",
+          contacted: false
+        },
+        {
+          name: "raven",
+          email: "raven3529427@163.cmo",
+          contacted: false
+        }
+      ]
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style>
+
+</style>
