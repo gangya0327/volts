381569d88bd415f241429f1420ef85060ede6cef
gangya0327 <king9999427@163.com>
2018-05-11 17:32:02 +0800

    todo

---
+++ vue-second/src/main.js
@@ -0,0 +1,98 @@
+// // The Vue build version to load with the `import` command
+// // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// import Vue from 'vue'
+// import App from './App'
+
+// Vue.config.productionTip = false
+
+// /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   components: { App },
+//   template: '<App/>'
+// })
+
+import 'todomvc-app-css/index.css'
+import Vue from 'vue'
+console.log('my todo vue start')
+
+var filters = {
+  all(todos) {
+    return todos
+  },
+  active(todos) {
+    return todos.filter((todo) => {
+      return !todo.completed
+    })
+  },
+  completed(todos) {
+    return todos.filter((todo) => {
+      return todo.completed
+    })
+  }
+}
+
+new Vue({
+  el: '.todoapp',
+  data: {
+    msg: 'helloworld',
+    newTodo: 'input something',
+    todos: [
+      { content: 'vue', completed: false },
+      { content: 'vuex', completed: false }
+    ],
+    editedTodo: []
+  },
+  computed: {
+    remain() {
+      return filters.active(this.todos).length
+    },
+    isAll: {
+      get() {
+        return this.remain === 0
+      },
+      set(value) {
+        this.todos.forEach((todo) => {
+          todo.completed = value
+        })
+      }
+    }
+  },
+  directives: {
+    focus(el, value) {
+      if (value) {
+        el.focus()
+      }
+    }
+  },
+  methods: {
+    addTodo(e) {
+      if(!this.newTodo){
+        return 
+      }
+      this.todos.push({
+        content: this.newTodo,
+        completed: false
+      })
+      this.newTodo = ''
+      console.log(this.todos)
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1)
+    },
+    editTodo(todo) {
+      this.editCache = todo.content
+      this.editedTodo = todo
+    },
+    doneEdit(todo, index) {
+      this.editedTodo = null
+      if(!todo.content){
+        this.removeTodo(index)
+      }
+    },
+    cancelEdit(todo){
+      this.editedTodo = null
+      todo.content = this.editCache
+    }
+  }
+})
